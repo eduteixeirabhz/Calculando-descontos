5844bf98af2ae077ef16e6e0a1fe4b312660ca40
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 13:04:26 -0300

    Add files via upload

---
+++ Desafio012.py
@@ -0,0 +1,10 @@
+#preço = float(input('Qual o valor da mercadoria? R$ '))
+#novo = preço*5/100
+#desconto = preço-novo
+#print("O preço com 5% de desconto é de R$ {:.2f} ".format(desconto))
+
+# ou
+
+preço = float(input('Qual o valor da mercadoria? R$ '))
+novo = preço-preço*5/100
+print("O preço com 5% de desconto é de R$ {:.2f} ".format(novo))
